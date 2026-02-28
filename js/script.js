ea4cb3c0c1ad6135b3ce6b888699feccbd68f4e2
okoba <taakiwami@gmail.com>
2026-02-28 20:11:35 +0900

    HTML,CSS実装完了

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-$(".p-mv").slick();
+// ハンバーガーメニュー
+
+const hum = document.getElementById('hamburger');
+const openNav = document.getElementById('open');
+
+hum.addEventListener('click', () => {
+    openNav.classList.toggle('active');
+})
