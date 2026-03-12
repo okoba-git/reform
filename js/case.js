306daf32f00ffd21951025b6cbfe445c2f0caa09
okoba <taakiwami@gmail.com>
2026-03-12 19:37:37 +0900

    施工事例JSアップデート

--- js/case.js
+++ js/case.js
@@ -2,7 +2,7 @@
 caseArray =
     [{ title: "猫とのびのび暮らす家", category: "戸建て", owner: "福岡市N様", img: "case1.jpg", alt: "福岡市のリフォーム事例。壁にキャットタワーがつけられており、黒猫がくつろいでいる。" },
     { title: "三世代で囲む食卓", category: "戸建て", owner: "福岡市T様", img: "case2.jpg", alt: "福岡市のリフォーム事例。白い部屋に机と椅子がある。机の上には食器類がおかれている。" },
-    { title: "水回りまとめてリフォーム", category: "集合住宅", owner: "古賀市S様", img: "img/case2.jpg", alt: "古賀市のリフォーム事例。白い壁の部屋。ブラウンの引き出しがある大きな洗面台がある。窓からは青空が見える。" }]
+    { title: "水回りまとめてリフォーム", category: "集合住宅", owner: "古賀市S様", img: "case3.jpg", alt: "古賀市のリフォーム事例。白い壁の部屋。ブラウンの引き出しがある大きな洗面台がある。窓からは青空が見える。" }]
 
 // 戸建て配列
 const houseArray = caseArray.filter(item => item.category === "戸建て");
@@ -18,28 +18,39 @@ const apartBtn = document.getElementById('apartment');
 const caseList = document.getElementById('case-list');
 
 // 描画
-const li = document.createElement('li');
+let li = document.createElement('li');
 caseList.append(li);
-caseArray.forEach(e => {
-    li.innerHTML = `<a href="case/case01.html"><img src="img/case1.jpg"alt="${e.alt}" class="p-case-card__img"></a><a href="#"><p>【${e.category}】${e.title}</p><p>${e.owner}</p></a>`
+li.setAttribute('class', 'p-case-card c-hover__text')
 
-});
+// 配列からHTMLを書く関数
+function updateCase(array) {
+    let html = '';
+    array.forEach(e => {
+        html += `<a href="case/case1.html"><img src="img/${e.img}"alt="${e.alt}" class="p-case-card__img"></a><a href="#"><p>【${e.category}】${e.title}</p><p>${e.owner}</p></a>`
+    });
+    li.innerHTML = html
+}
+
+updateCase(caseArray);
 
 
 // クリックイベント
 allBtn.addEventListener('click', () => {
+    updateCase(caseArray);
     allBtn.style.background = "#B0D850";
     houseBtn.style.background = "none";
     apartBtn.style.background = "none";
 })
 
 houseBtn.addEventListener('click', () => {
+    updateCase(houseArray);
     houseBtn.style.background = "#B0D850";
     allBtn.style.background = "none";
     apartBtn.style.background = "none";
 })
 
 apartBtn.addEventListener('click', () => {
+    updateCase(apartArray);
     apartBtn.style.background = "#B0D850";
     houseBtn.style.background = "none";
     allBtn.style.background = "none";
