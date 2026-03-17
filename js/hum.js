971babe965514557c01ad9d9848f8e07a5a79bf5
Sawako Okoba <taakiwami@gmail.com>
2026-03-17 15:49:47 +0900

    common.jsへファイル名変更、読み込みファイル名訂正

--- js/hum.js
+++
@@ -1,8 +0,0 @@
-// ハンバーガーメニュー
-
-const hum = document.getElementById('hamburger');
-const openNav = document.getElementById('open');
-
-hum.addEventListener('click', () => {
-    openNav.classList.toggle('active');
-})
