e6da945b49e7224b31da091c828933ead7abab1d
Sawako Okoba <taakiwami@gmail.com>
2026-03-18 11:24:18 +0900

    リファクタリング

--- js/common.js
+++ js/common.js
@@ -7,11 +7,16 @@ hum.addEventListener('click', () => {
     openNav.classList.toggle('active');
 })
 
+// スクロールアニメの関数
+function scrollAnime(className) {
+    // スクロールアニメーションを付けたい要素を取得
+    const items = document.querySelectorAll(`${className}`);
+    // それぞれの要素を監視する
+    items.forEach(item => {
+        observer.observe(item);
+    });
+}
 
-// スクロールアニメーションを付けたい要素を取得
-const titles = document.querySelectorAll(".c-title");
-const processImgs = document.querySelectorAll(".p-process__img");
-const subCopy = document.querySelectorAll('.subcopy')
 
 // オプション設定
 const option = {
@@ -23,31 +28,21 @@ const option = {
 // コールバック関数とオプションを渡します
 const observer = new IntersectionObserver(doWhenIntersect, option);
 
-
-// それぞれの要素を監視する
-titles.forEach(title => {
-    observer.observe(title);
-});
-processImgs.forEach(img => {
-    observer.observe(img);
-})
-subCopy.forEach(item => {
-    observer.observe(item);
-})
-
-
 // コールバック関数
 function doWhenIntersect(entries) {
     entries.forEach(entry => {
-
         // 要素が交差したら…
         if (entry.isIntersecting) {
             // クラスをつける
             entry.target.classList.add('animation');
-
         } else {
             // クラスを外す
             entry.target.classList.remove('animation');
         }
     });
 }
+
+
+scrollAnime('.c-title');
+scrollAnime(".p-process__img");
+scrollAnime('.subcopy');
