088bb5ad9e8d0fc21b026e2628cefab6a3f87fce
okoba <taakiwami@gmail.com>
2026-03-21 13:04:35 +0900

    リファクタリング

--- js/case.js
+++ js/case.js
@@ -4,10 +4,6 @@ const caseArray =
     { title: "三世代で囲む食卓", category: "戸建て", owner: "福岡市T様", img: "case2.jpg", alt: "福岡市のリフォーム事例。白い部屋に机と椅子がある。机の上には食器類がおかれている。" },
     { title: "水回りまとめてリフォーム", category: "集合住宅", owner: "古賀市S様", img: "case3.jpg", alt: "古賀市のリフォーム事例。白い壁の部屋。ブラウンの引き出しがある大きな洗面台がある。窓からは青空が見える。" }]
 
-// 戸建て配列
-const houseArray = caseArray.filter(item => item.category === "戸建て");
-// 集合住宅配列
-const apartArray = caseArray.filter(item => item.category === "集合住宅");
 
 // ボタンのID取得
 const allBtn = document.getElementById('all-case');
@@ -19,41 +15,39 @@ const caseList = document.getElementById('case-list');
 
 
 // 配列からHTMLを書く関数
-function updateCase(array) {
+function renderCase(array) {
     let html = '';
     array.forEach(e => {
-        html += `<li class="p-case__card c-hover__text"><a href="case/case1.html"><img src="img/${e.img}"alt="${e.alt}" class="p-case__card-img"></a><a href="#"><p>【${e.category}】${e.title}</p><p>${e.owner}</p></a></li>`
+        html += `<a href="case/case1.html"><img src="img/${e.img}"alt="${e.alt}" class="p-case__card-img"></a><a href="case/case1.html"><p>【${e.category}】${e.title}</p><p>${e.owner}</p></a>`
     });
     caseList.innerHTML = html
 }
 
-// ボタンの背景色を変える関数
-function changeBackColor(btnName) {
-    allBtn.style.background = "none";
-    houseBtn.style.background = "none";
-    apartBtn.style.background = "none";
-    btnName.style.background = "#B0D850";
+
+// クリックしたときの関数
+function clickFunc(btn, category) {
+    btn.addEventListener('click', () => {
+        if (category === 'すべて') {
+            renderCase(caseArray);
+        } else {
+            // 該当の配列作成
+            const array = caseArray.filter(item => item.category === category);
+            // 描画
+            renderCase(array);
+        }
+        // ボタンの色を変える
+        allBtn.style.background = "";
+        houseBtn.style.background = "";
+        apartBtn.style.background = "";
+        btn.style.background = "#B0D850";
+    })
+
 }
 
-
-
 // デフォルト表示
-updateCase(caseArray);
+allBtn.style.background = "#B0D850";
+renderCase(caseArray);
 
-
-
-// クリックイベント
-allBtn.addEventListener('click', () => {
-    updateCase(caseArray);
-    changeBackColor(allBtn);
-})
-
-houseBtn.addEventListener('click', () => {
-    updateCase(houseArray);
-    changeBackColor(houseBtn);
-})
-
-apartBtn.addEventListener('click', () => {
-    updateCase(apartArray);
-    changeBackColor(apartBtn);
-})
+clickFunc(allBtn, 'すべて');
+clickFunc(houseBtn, '戸建て');
+clickFunc(apartBtn, '集合住宅');
