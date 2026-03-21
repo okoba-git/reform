886f81ad2855d13c789a5555851980e57c143c06
okoba <taakiwami@gmail.com>
2026-03-21 13:32:38 +0900

    施工事例リンクを動的表示

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-// お知らせの情報
-const allArray = [
-    { category: 'お知らせ', date: '2025-12-24', title: '冬季休業期間のお知らせ' },
-    { category: 'お知らせ', date: '2025-10-08', title: 'マンションリフォーム施工事例を追加しました' },
-    { category: 'お得な情報', date: '2025-08-03', title: '水回りリフォーム相談会のご案内' },
-    { category: 'お知らせ', date: '2025-05-20', title: '営業時間が変更になります' },
-    { category: 'お得な情報', date: '2025-05-20', title: '春のリフォームキャンペーン' },
-    { category: 'お得な情報', date: '2025-04-10', title: '【補助金活用】窓断熱リフォーム相談会スタート' },
-    { category: 'お知らせ', date: '2025-03-15', title: '一級建築士による無料相談会の空き状況' },
-    { category: 'お得な情報', date: '2025-02-01', title: '新生活応援！内装壁紙貼り替えフェア！' },
-    { category: 'お知らせ', date: '2025-01-10', title: '公式サイトをリニューアルいたしました' }
-];
-
 // トップページ
+// ニュース
 // dl要素取得（ここにHTMLを書く）
 const topNewsList = document.getElementById('top-news-area');
 
-let topPageHtml = '';
+let topNewsHtml = '';
 for (let i = 0; i < 3; i++) {
-    topPageHtml += `<div class="l-top-news__item"><dt class="l-top-news__item--head"><p class="c-news-item__icon fz12">${allArray[i].category}</p>${allArray[i].date}</dt><dd class="c-hover__text"><a href="#">${allArray[i].title}</a></dd></div>`;
+    topNewsHtml += `<div class="l-top-news__item"><dt class="l-top-news__item--head"><p class="c-news-item__icon fz12">${newsListArray[i].category}</p>${newsListArray[i].date}</dt><dd class="c-hover__text"><a href="#">${newsListArray[i].title}</a></dd></div>`;
 }
-topNewsList.innerHTML = topPageHtml;
+topNewsList.innerHTML = topNewsHtml;
+
+
+// 施工事例
+
+// ここに施工事例のHTMLを書く
+const topCaseArea = document.getElementById('js-top-case');
+
+let topCaseHtml = '';
+for (let i = 0; i < 3; i++) {
+    topCaseHtml += `<li class="p-case__card c-hover__text"><a href="case/case1.html"><img src="img/${caseArray[i].img}"alt="${caseArray[i].alt}" class="p-case__card-img"></a><a href="case/case1.html"><p>【${caseArray[i].category}】${caseArray[i].title}</p><p>${caseArray[i].owner}</p></a></li>`;
+}
+topCaseArea.innerHTML = topCaseHtml;
